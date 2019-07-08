2f3b5ca5d603a0133a52c5d9ddd6c9f40bc0f9f6
andixboya <andreyanboyadzhiev@gmail.com>
2019-07-08 23:51:56 +0300

    Committing 1nd and 2nd tasks from Reflection and Attributes exercises

---
+++ 7.Reflection and Attributes/1.Harvesting Fields/HarvestingFieldsTest.cs
@@ -0,0 +1,89 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+
+            var assembly = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.Name == "HarvestingFields");
+
+
+            StringBuilder sb = new StringBuilder();
+
+            string input = Console.ReadLine();
+
+            var fields = assembly.GetFields(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+
+            while (input != "HARVEST")
+            {
+
+                switch (input)
+                {
+                    case "private":
+                        foreach (var f in fields.Where(f => f.IsPrivate))
+                        {
+                            sb.AppendLine($"{input} {f.FieldType.Name} {f.Name}");
+                        }
+                        break;
+
+                    case "protected":
+
+                        foreach (var f in fields.Where(f => f.IsFamily))
+                        {
+                            sb.AppendLine($"{input} {f.FieldType.Name} {f.Name}");
+                        }
+
+                        break;
+
+                    case "public":
+                        foreach (var f in fields.Where(f => f.IsPublic))
+                        {
+                            sb.AppendLine($"{input} {f.FieldType.Name} {f.Name}");
+                        }
+                        break;
+
+                    case "all":
+
+                        foreach (var f in fields)
+                        {
+                            string typeOfModifier = GetModifier(f);
+
+                            sb.AppendLine($"{typeOfModifier} {f.FieldType.Name} {f.Name}");
+
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+
+        }
+
+        private static string GetModifier(FieldInfo f)
+        {
+
+
+            if (f.IsPublic)
+            {
+                return "public";
+            }
+            else if (f.IsFamily)
+            {
+                return "protected";
+            }
+            else
+            {
+                return "private";
+            }
+
+        }
+    }
+}
---
+++ 7.Reflection and Attributes/2.Black Box Integer/BlackBoxIntegerTests.cs
@@ -0,0 +1,40 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+
+            var methods = typeof(BlackBoxInteger).GetMethods(BindingFlags.NonPublic|BindingFlags.Instance);
+
+
+            string input = Console.ReadLine();
+
+            var instance = Activator.CreateInstance(typeof(BlackBoxInteger),true) ;
+            var field = instance.GetType().GetField("innerValue", BindingFlags.Instance| BindingFlags.NonPublic);
+
+            while (input!="END")
+            {
+                string[] partitions = input.Split("_", StringSplitOptions.RemoveEmptyEntries);
+                string command = partitions[0];
+                int value = int.Parse(partitions[1]);
+
+                var method = methods.FirstOrDefault(m => m.Name == command);
+
+                
+                 method.Invoke(instance,new object[] { value });
+
+                Console.WriteLine(field.GetValue(instance)); 
+                
+
+                input = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
