d3ad1e4ef6b16b81ab412470b9c3182060ceb403
andixboya <andreyanboyadzhiev@gmail.com>
2019-07-09 08:42:06 +0300

    Committing 3-rd and 4th tasks from Reflection and Attributes exercises

---
+++ 7.Reflection and Attributes/3.Barrack wars- a new factory/Core/Factories/UnitFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace _03BarracksFactory.Core.Factories
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Contracts;
+
+    public class UnitFactory : IUnitFactory
+    {
+        public IUnit CreateUnit(string unitType)
+        {
+            var type = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.Name == unitType);
+
+            IUnit instance =  (IUnit) Activator.CreateInstance(type, null);
+
+            if (instance is null)
+            {
+                throw new ArgumentException($"{unitType} is not valid!");
+            }
+
+
+            return instance;
+
+        }
+    }
+}
